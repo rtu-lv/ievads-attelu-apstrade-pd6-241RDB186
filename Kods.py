ad9846439f9f139b97f8c4542542ede186694ec9
Martins Zariņš <hello@zarins.zip>
2026-05-17 23:34:57 +0300

    Roughly implemented assignment.

--- Kods.py
+++ Kods.py
@@ -32,23 +32,48 @@ def gaussian_blur(img, *, kernel):
 	cv2.GaussianBlur(img, (kernel, kernel), 0, dst=img)
 	return img
 
-def otsu_thresh(img):
-	grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+def greyscale(img):
+	return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-	_, mask = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-	return mask
+def log_correction(img, *, norm):
+	data = np.float32(img) / 255
+	data[:] = np.clip(norm * np.log(1 + data), 0, 1)
 
-def morph_open(mask, *, kernel):
-	struct = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel, kernel))
-	return cv2.morphologyEx(mask, cv2.MORPH_OPEN, struct)
+	img[:] = np.uint8(data * 255)
+	return img
+
+def otsu_thresh(grey):
+	cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU, dst=grey)
+	return grey
+
+def morph_open(grey, *, kernel, shape=cv2.MORPH_ELLIPSE):
+	struct = cv2.getStructuringElement(shape, (kernel, kernel))
+
+	cv2.morphologyEx(grey, cv2.MORPH_OPEN, struct, dst=grey)
+	return grey
+
+def as_mask(grey):
+	mask = grey == 255
+	return mask[:, :, np.newaxis]
+
+
+def portrait_effect(img, *, blur=21):
+	blur = gaussian_blur(img.copy(), kernel=blur)
+
+	grey = greyscale(img)
+	log_correction(grey, norm=10)
+
+	otsu_thresh(grey)
+	morph_open(grey, kernel=20)
+
+	mask = as_mask(grey)
+	img[:] = np.where(mask, blur, img)
+	return img
 
 
 A = read('Bildes/Vilciens.jpg')
 plot(A, 'Vilciens', save=False)
 
-B = otsu_thresh(A)
-plot(B, 'Vilciens - Otsu segmentācija')
-
-B = morph_open(B, kernel=5)
-plot(B, 'Vilciens - Otsu segmentācija - Morfoloģiskā atveršana', show=True)
+portrait_effect(A)
+plot(A, 'Vilciens - Portreta efekts', show=True)
 
