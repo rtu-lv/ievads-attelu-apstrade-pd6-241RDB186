071bb53c1d9719d7c41201dc5cf0bd71ce73e655
Martins Zariņš <hello@zarins.zip>
2026-05-17 20:26:47 +0300

    Added Otsu thresholding and morphological opening.

--- Kods.py
+++ Kods.py
@@ -12,7 +12,7 @@ def read(path):
 	cv2.cvtColor(img, cv2.COLOR_BGR2RGB, dst=img)
 	return img
 
-def plot(img, title, save=os.getenv("SCRIPT_SAVE_IMG") is not None, show=False):
+def plot(img, title=None, save=os.getenv("SCRIPT_SAVE_IMG") is not None, show=False):
 	if save:
 		cv2.cvtColor(img, cv2.COLOR_RGB2BGR, dst=img)
 		cv2.imwrite(f'Figūras/{ title }.jpg', img)
@@ -27,14 +27,28 @@ def plot(img, title, save=os.getenv("SCRIPT_SAVE_IMG") is not None, show=False):
 	if show:
 		plt.show()
 
-def gaussian_blur(img, kernel=5):
+
+def gaussian_blur(img, *, kernel):
 	cv2.GaussianBlur(img, (kernel, kernel), 0, dst=img)
 	return img
+
+def otsu_thresh(img):
+	grey = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+	_, mask = cv2.threshold(grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	return mask
+
+def morph_open(mask, *, kernel):
+	struct = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel, kernel))
+	return cv2.morphologyEx(mask, cv2.MORPH_OPEN, struct)
 
 
 A = read('Bildes/Vilciens.jpg')
 plot(A, 'Vilciens', save=False)
 
-B = gaussian_blur(A)
-plot(B, 'Vilciens', show=True)
+B = otsu_thresh(A)
+plot(B)
+
+B = morph_open(B, kernel=5)
+plot(B, show=True)
 
