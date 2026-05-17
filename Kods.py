425b7c93e70fe75d5a509a61d19ec03126112bfe
Martins Zariņš <hello@zarins.zip>
2026-05-17 17:50:09 +0300

    Modified read and plot logic. Added gaussian blur.

--- Kods.py
+++ Kods.py
@@ -8,11 +8,13 @@ import matplotlib.pyplot as plt
 
 def read(path):
 	img = cv2.imread(path)
-	return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def plot(img, title, save=os.getenv('SCRIPT_SAVE_IMG') is not None):
+	cv2.cvtColor(img, cv2.COLOR_BGR2RGB, dst=img)
+	return img
+
+def plot(img, title, save=os.getenv("SCRIPT_SAVE_IMG") is not None, show=False):
 	if save:
-		img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+		cv2.cvtColor(img, cv2.COLOR_RGB2BGR, dst=img)
 		cv2.imwrite(f'Figūras/{ title }.jpg', img)
 		return
 
@@ -21,5 +23,17 @@ def plot(img, title, save=os.getenv('SCRIPT_SAVE_IMG') is not None):
 	plt.title(title)
 	plt.axis(False)
 
-	plt.show()
+	if show:
+		plt.show()
+
+def gaussian_blur(img, kernel=5):
+	cv2.GaussianBlur(img, (kernel, kernel), 0, dst=img)
+	return img
+
+
+A = read('Bildes/Vilciens.jpg')
+plot(A, 'Vilciens', save=False)
+
+B = gaussian_blur(A)
+plot(B, 'Vilciens', show=True)
 
